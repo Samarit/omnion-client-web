68f2cfacd2a445fe34dbcde68383485d452beee5
Samarit <kislyakaleksey@gmail.com>
2024-06-25 22:07:29 +0300

    drafting response logic

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -2,9 +2,9 @@
 
 import { Button } from "@/shared/ui/button"
 import { Input } from "@/shared/ui/input"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Socket, io } from "socket.io-client"
-import ChatBox from "./chatbox"
+import { ChatBox } from "./chatbox"
 
 const sendSocketMessage = (socket: Socket, message: string) => {
   if (!socket) {
@@ -15,11 +15,19 @@ const sendSocketMessage = (socket: Socket, message: string) => {
   socket.emit("message", message)
 }
 
+interface IMessage {
+  text: string
+  position: "left" | "right"
+}
+
 export default function Chat() {
   const [socket, setSocket] = useState<Socket | null>(null)
   const [connected, setConnected] = useState(false)
-  const [message, setMessage] = useState("")
-  const [chatHistory, setChatHistory] = useState<string[][]>([])
+  const [messageToSend, setMessageToSend] = useState("")
+  const [chatHistory, setChatHistory] = useState<IMessage[]>([])
+  const [pendingResponse, setPendingResponse] = useState(false)
+  const [draftingResponse, setDraftingResponse] = useState<string>("")
+  const draftingResponseRef = useRef(draftingResponse)
 
   useEffect(() => {
     setSocket(
@@ -37,9 +45,10 @@ export default function Chat() {
         return
       }
       if (e.key === "Enter") {
-        sendSocketMessage(socket, message)
-        console.log("message", message)
-        setMessage("")
+        sendSocketMessage(socket, messageToSend)
+        setPendingResponse(true)
+        console.log("messageToSend", messageToSend)
+        setMessageToSend("")
       }
     }
 
@@ -48,7 +57,7 @@ export default function Chat() {
     return () => {
       window.removeEventListener("keydown", enterHandler)
     }
-  }, [message])
+  }, [messageToSend])
 
   useEffect(() => {
     if (!socket) return
@@ -61,16 +70,28 @@ export default function Chat() {
 
     socket.on("response:start", () => {
       console.log("response:start")
-      setChatHistory((prev) => [...prev, []])
+      setPendingResponse(false)
     })
 
-    socket.on("response:chunk", (message: any) => {
+    socket.on("response:chunk", (chunk: string) => {
       console.log("response:chunk")
-      setChatHistory((prev) => {
-        const newHistory = [...prev]
-        newHistory[newHistory.length - 1].push(message)
-        return newHistory
-      })
+      setDraftingResponse(draftingResponseRef.current + chunk)
+      draftingResponseRef.current += chunk
+    })
+
+    socket.on("response:end", () => {
+      console.log("response:end")
+      const response = draftingResponseRef.current
+      setChatHistory((prev) => [
+        ...prev,
+        {
+          text: response,
+          position: "left",
+        },
+      ])
+
+      setDraftingResponse("")
+      draftingResponseRef.current = ""
     })
 
     socket.on("disconnect", () => {
@@ -86,27 +107,37 @@ export default function Chat() {
   return (
     <div className='chat'>
       <h3>{connected ? "connected!" : "connecting..."}</h3>
+
       <ul className='flex flex-col w-full gap-2'>
         {chatHistory.map((msg, i) => (
-          <ChatBox
-            position={i % 2 === 0 ? "left" : "right"}
-            message={msg.join("")}
-            key={i}></ChatBox>
+          <ChatBox position={msg.position} message={msg.text} key={i}></ChatBox>
         ))}
+
+        {(pendingResponse || draftingResponse) && (
+          <ChatBox
+            position={"left"}
+            message={draftingResponse}
+            pending={pendingResponse}
+          />
+        )}
       </ul>
+
       <Input
-        value={message}
+        value={messageToSend}
         onChange={(e) => {
-          setMessage(e.target.value)
+          setMessageToSend(e.target.value)
         }}
         placeholder='Enter message...'
       />
+
       <Button
         variant={"outline"}
         onClick={() => {
-          if (!message || !socket) return
-          sendSocketMessage(socket, message)
-          setMessage("")
+          if (!messageToSend || !socket) return
+
+          sendSocketMessage(socket, messageToSend)
+          setPendingResponse(true)
+          setMessageToSend("")
         }}>
         Send
       </Button>
--- src/components/chat/chatbox.tsx
+++ src/components/chat/chatbox.tsx
@@ -1,14 +1,17 @@
-export default function ChatBox(props: {
+interface ChatBoxProps {
   position: "left" | "right"
   message: string
-}) {
+  pending?: boolean
+}
+
+export function ChatBox(props: ChatBoxProps) {
   return (
     <li
       className='chatbox w-3/4 p-2 border-2 border-solid border-white rounded-sm'
       style={{
         alignSelf: props.position === "left" ? "flex-start" : "flex-end",
       }}>
-      {props.message}
+      {props.pending ? "..." : props.message}
     </li>
   )
 }
